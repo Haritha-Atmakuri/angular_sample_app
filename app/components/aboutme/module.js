32565e16921890acee960e37bcf9e7965074ee1b
Haritha-Atmakuri <harithaatmakuri1105@gmail.com>
2019-05-22 16:29:07 -0500

    added index.html and module.js

---
+++ app/components/aboutme/module.js
@@ -0,0 +1,19 @@
+let myapp= angular.module("Myapp",[]);
+
+let mycontroller = function($scope){
+    $scope.message="Hello World";
+}
+
+myapp.controller("MyController",mycontroller);
+
+let JSONController = function($scope){
+    let Student={
+        FirstName : "Haritha",
+        LastName : "Atmakuri",
+        Email: "S534049@nwmissouri.edu"
+    }
+
+    $scope.Student=Student;
+}
+
+myapp.controller("Student",JSONController);
